c59959348a801e6e58f009e87a110aee56cd805c
Anuoluwapo25 <anuoluwapoali25@gmail>
2024-06-13 11:50:17 +0000

    sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,9 @@
 -- creates a table users
 -- with attributes (id, email, name)
 
-CREATE TABLE IF NOT EXISTS users {
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
-    email STRING(255) NOT NULL UNIQUE,
-    name STRING(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
     PRIMARY KEY (id)
-};
+);
