b823dcfcbb74de520c6cef4aa9850e427e030749
Anuoluwapo25 <anuoluwapoali25@gmail>
2024-06-13 13:04:04 +0000

    band fans updated

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,5 @@
 -- creates a table users
 -- script that ranks country origins of bands ordered by number
-
-
-SELECT origin, COUNT(*) AS nb_fans
-FROM metal_bands
+SELECT DISTINCT origin, SUM(fans) AS nb_fans FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
